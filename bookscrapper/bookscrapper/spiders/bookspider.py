a8e469f705d639b8721b23f4587bb53676f49a80
Damithedev <adamidogo@gmail.com>
2023-09-12 03:27:04 +0100

    scrape

--- bookscrapper/bookscrapper/spiders/bookspider.py
+++ bookscrapper/bookscrapper/spiders/bookspider.py
@@ -18,11 +18,7 @@ class BookspiderSpider(scrapy.Spider):
 
             yield response.follow(book_url, callback=self.parse_book_page)
 
-            yield {
-                'name': book.css('h3 a::text').get(),
-                'price': book.css('.product_price .price_color::text').get(),
-                'url': book.css('h3 a::attr(href)').get()  # Use ::attr(href) to extract the 'href' attribute
-            }
+
         next_page =  response.css('li.next a ::attr(href)').get()
         print(next_page)
         if next_page is not None:
@@ -34,4 +30,21 @@ class BookspiderSpider(scrapy.Spider):
             yield response.follow(next_page_url, callback=self.parse)
 
     def parse_book_page(self, response):
+        table_rows = response.css("table tr")
+        yield {
+            'url': response.url,
+            'title': response.css('.product_main h1::text').get(),
+            'product_type': table_rows[1].css('td::text').get(),
+            'price_excl_tax':table_rows[2].css("td::text").get(),
+            'price_incl_tax': table_rows[3].css("td::text").get(),
+            'tax':table_rows[4].css("td::text").get(),
+            'availability': table_rows[5].css("td::text").get(),
+            'num_reveiws': table_rows[6].css("td::text").get(),
+            'stars': response.css('p.star-rating ::attr(class)').get(),
+            'category':response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+            'description':  response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+            'price': response.css('p,price_color ::text').get()
+
+
+        }
         pass
