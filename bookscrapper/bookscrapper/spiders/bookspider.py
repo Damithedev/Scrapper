8ef0c9c949cd2ea4d1efe4b5a5eb947b2e67133c
Damithedev <adamidogo@gmail.com>
2023-09-13 04:58:08 +0100

    sigh

--- bookscrapper/bookscrapper/spiders/bookspider.py
+++ bookscrapper/bookscrapper/spiders/bookspider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from bookscrapper.items import BookItem
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
@@ -18,8 +18,7 @@ class BookspiderSpider(scrapy.Spider):
 
             yield response.follow(book_url, callback=self.parse_book_page)
 
-
-        next_page =  response.css('li.next a ::attr(href)').get()
+        next_page = response.css('li.next a ::attr(href)').get()
         print(next_page)
         if next_page is not None:
             if 'catalogue/' in next_page:
@@ -31,20 +30,20 @@ class BookspiderSpider(scrapy.Spider):
 
     def parse_book_page(self, response):
         table_rows = response.css("table tr")
-        yield {
-            'url': response.url,
-            'title': response.css('.product_main h1::text').get(),
-            'product_type': table_rows[1].css('td::text').get(),
-            'price_excl_tax':table_rows[2].css("td::text").get(),
-            'price_incl_tax': table_rows[3].css("td::text").get(),
-            'tax':table_rows[4].css("td::text").get(),
-            'availability': table_rows[5].css("td::text").get(),
-            'num_reveiws': table_rows[6].css("td::text").get(),
-            'stars': response.css('p.star-rating ::attr(class)').get(),
-            'category':response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
-            'description':  response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
-            'price': response.css('p.price_color ::text').get()
+        book_item = BookItem()
 
+        book_item['url'] = response.url,
+        book_item['title'] = response.css('.product_main h1::text').get(),
+        book_item['product_type'] = table_rows[1].css('td::text').get(),
+        book_item['price_excl_tax'] = table_rows[2].css("td::text").get(),
+        book_item['price_incl_tax'] = table_rows[3].css("td::text").get(),
+        book_item['tax'] = table_rows[4].css("td::text").get(),
+        book_item['availability'] = table_rows[5].css("td::text").get(),
+        book_item['num_reviews'] = table_rows[6].css("td::text").get(),
+        book_item['stars'] = response.css('p.star-rating ::attr(class)').get(),
+        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+        book_item['price'] = response.css('p.price_color ::text').get()
 
-        }
+        yield book_item
         pass
